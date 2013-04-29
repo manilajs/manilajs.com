f1ab42ffad220f50f9c40cccdf0cc9aa6f7ecd5a
Rico Sta. Cruz <hi@ricostacruz.com>
2013-04-29 11:41:43 +0800

    Registration open.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 ;(function($){
-  $('.picture-header').fillsize('> .wallpaper');
+  $(function() {
+    $('.picture-header').fillsize('> .wallpaper');
+  });
 
   function onImageLoad($img, fn) {
     if ($img[0].complete) { fn.apply($img, []); }
@@ -7,13 +9,17 @@
   }
 
   // Fade on load
-  onImageLoad($('img.wallpaper'), function() {
-    var $parent = $(this).closest('.picture-header');
-    setTimeout(function() { $parent.addClass('loaded'); }, 25);
+  $(function() {
+    onImageLoad($('img.wallpaper'), function() {
+      var $parent = $(this).closest('.picture-header');
+      setTimeout(function() { $parent.addClass('loaded'); }, 25);
+    });
   });
 
   // Prevent text orphans
-  $('.next-meetup h5, .unorphan, .brief p, .speaker p, .closing p, .about-us p').unorphan();
+  $(function() {
+    $('.next-meetup h5, .unorphan, .brief p, .speaker p, .closing p, .about-us p').unorphan();
+  });
 
   // Disable clicks on dead links
   $(document).on('click', '[href=#]', function(e) {
