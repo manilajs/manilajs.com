749544258ba504d6a68f3ca9f366273303f583f8
Rico Sta. Cruz <hi@ricostacruz.com>
2014-07-02 20:00:11 +0800

    New subscribe footer.

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,20 @@ $(function() {
 $(document).on('click', '[href=#]', function(e) {
   e.preventDefault();
 });
+
+$(function () {
+  $('form[role~="validate"]').on('submit', function (e) {
+    var $requireds = $(this).find('input[required]');
+    var $invalids =
+    $requireds.filter(function (i, input) {
+      return typeof input.value !== 'string' || input.value.length === 0;
+    });
+
+    if ($invalids.length > 0) {
+      alert("Please fill up all required fields.");
+      $invalids.eq(0).focus();
+      e.preventDefault();
+      return false;
+    }
+  });
+});
