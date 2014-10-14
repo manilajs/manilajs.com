aa7288c055dad76077d9fb98e304d7ad24fe3123
Rico Sta. Cruz <hi@ricostacruz.com>
2014-10-14 14:21:04 +0800

    Simplify hero shot look.

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,3 @@
-$(function() {
-  $('.picture-header').fillsize('> .wallpaper');
-});
-
-// Fade on load
-$(function() {
-  onImageLoad($('img.wallpaper'), function() {
-    var $parent = $(this).closest('.picture-header');
-    $(window).trigger('resize');
-    setTimeout(function() { $parent.addClass('loaded'); }, 25);
-  });
-
-  function onImageLoad($img, fn) {
-    if ($img[0].complete) { fn.apply($img, []); }
-    else { $img.on('load', fn); }
-  }
-});
-
 // Prevent text orphans
 $(function() {
   $('.next-meetup h5, .unorphan, .brief p, .speaker p, .closing p, .about-us p').unorphan();
